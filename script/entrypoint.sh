2b92442ea412ec92416a777d1beda8a33f099fd6
imkehno <v.kortelai@gmail.com>
2020-05-20 12:49:23 +0300

    Add comment for testing for airomi

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -8,6 +8,7 @@
 TRY_LOOP="20"
 
 # Global defaults and back-compat
+# Possibly /test_dags/ for our use case
 : "${AIRFLOW_HOME:="/usr/local/airflow"}"
 : "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
 : "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}"
